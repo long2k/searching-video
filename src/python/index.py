5a6fafd1d379409f3d92eb0d5c1e05901a8863f1
long2k <truongquanglong777@gmail.com>
2022-06-19 15:55:04 +0700

    update 256

--- src/python/index.py
+++ src/python/index.py
@@ -11,7 +11,7 @@ def get_LBP(path, url):
     img = cv2.resize(img,(256,256))
     image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     lbp_image = local_binary_pattern(image, 8, 1, method='default')
-    hist, _ = numpy.histogram(lbp_image.ravel(), density=True, bins=256, range=(0, 255))
+    hist, _ = numpy.histogram(lbp_image.ravel(), density=True, bins=256, range=(0, 256))
     save_vector.save_vectors(hist, url)
     return True
 
